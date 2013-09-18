88ca3183128ca5f3d1d1f92a38a61dc45b9529bd
Fennic <carinadelorenzo@gmail.com>
2013-09-18 09:13:02 -0700

    Created Eclipse Project

---
+++ src/Grid.java
@@ -0,0 +1,17 @@
+
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class Grid {
+	public static void main(String[] args){
+		
+	}
+	
+	public Grid() {
+	       init();
+	}
+	
+	public void init(){
+		setSize();
+	}
+}
